6a4522bc4dbe20e02302a0ad1dc5b5b65c277703
Michael Russell <mrussell@tombras.com>
2016-05-18 10:29:38 -0400

    init commit

---
+++ build/main-dist-dist.js
@@ -0,0 +1,16 @@
+'use strict';
+
+System.register([], function (_export, _context) {
+  var test;
+  return {
+    setters: [],
+    execute: function () {
+      test = function test() {
+        return console.log('test');
+      };
+    }
+  };
+});
+
+//# sourceMappingURL=main-dist.js.map
+
